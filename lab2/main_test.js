0cc8e649f91226b59bea8665f947d6c9e352313b
CTHua <illiew2470@gmail.com>
2025-03-20 15:35:07 +0800

    feat: lab3

--- lab2/main_test.js
+++ lab2/main_test.js
@@ -1,52 +1,6 @@
 const test = require('node:test');
 const assert = require('assert');
-const fs = require('fs');
-
-test.mock.method(fs, 'readFile', (path,encoding,callback)=>{callback(0,'Alice\nBob\nCharlie')});
 const { Application, MailSystem } = require('./main');
-test('Test MailSystem write', () => {
-    const testmail = new MailSystem();
-    assert.deepStrictEqual(testmail.write('Alice'), 'Congrats, ' + 'Alice' + '!');
-});
-test('Test MailSystem send', () => {
-    const testmail = new MailSystem();
-    test.mock.method(Math,'random',() => true);
-    assert.deepStrictEqual(testmail.send('Alice', 'success'), true);
-    test.mock.method(Math,'random',() => false);
-    assert.deepStrictEqual(testmail.send('Alice', 'fail'), false);
-});
 
-test('Test Application constructor', async () => {
-    const app = new Application();
-    await app.getNames();
-    assert.deepStrictEqual(app.people, ['Alice','Bob','Charlie']);
-    assert.deepStrictEqual(app.selected, []);
-});
-
-
-test('Test Application getRandomPerson', async () => {
-    const app = new Application();
-    await app.getNames();
-    test.mock.method(Math,'random',() => 0);
-    assert.deepStrictEqual(app.getRandomPerson(), app.people[0]);
-});
-
-test('Test Application selectNextPerson', async () => {
-    const app = new Application();
-    await app.getNames();
-    let i = 0;
-    test.mock.method(app, 'getRandomPerson', ()=>{
-        return app.people[i++]});
-    assert.deepStrictEqual(app.selectNextPerson(), app.people[0]);
-    i = 0;
-    assert.deepStrictEqual(app.selectNextPerson(), app.people[1]);
-    app.selected = [0, 0, 0];
-    assert.deepStrictEqual(app.selectNextPerson(), null);
-});
-
-test('Test Application notifySelected()', async () => {
-    const app = new Application();
-    await app.getNames();
-    app.selected = ['Alice'];
-    assert.deepStrictEqual(app.notifySelected(), undefined);
-});
+// TODO: write your tests here
+// Remember to use Stub, Mock, and Spy when necessary
