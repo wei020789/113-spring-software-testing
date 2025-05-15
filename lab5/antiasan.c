0bb0894b09d1001df71c6d6d596c23dcd509d93c
YingMuo <wl03452329@gmail.com>
2025-05-15 12:29:31 +0800

    feat: lab8

--- lab5/antiasan.c
+++ lab5/antiasan.c
@@ -1,10 +1,6 @@
 #include <string.h>
 
-void antiasan(unsigned long addr) {
-    // The address of the shadow memory is 0x7fff8000
-    unsigned long shadow   = ((addr + 0x87 ) >> 3) + 0x7fff8000;
+void antiasan(unsigned long addr)
+{
 
-    for (int i = 0; i < 0x10; i += 1) {
-        *(char *)(shadow + i) = 0;
-    }
 }
